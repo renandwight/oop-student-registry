f74a19228d30da3255d8cc63e25b5106d4a04709
Chris Sorley <inc3ndi4ry@gmail.com>
2025-10-14 20:43:06 -0500

    Feature Student Registry: reworked setters for all attributes.  Tests related to age and grade for some reason did not like the additional sub functions and had to rewrite logic.  Now it passes all tests.

--- student_registry.py
+++ student_registry.py
@@ -1,68 +1,70 @@
 class Student:
-    def __init__(self, name, age, grade):
+    def __init__(self, name, age = 13, grade = "12th"):
         self.name = name
         self.age = age
         self.grade = grade
 
     @property
     def name(self):
-        return {self._name}
+        return self._name
     
-    @name.setter        # Name >= 3 chars no spaces or special chars; format (Firstname Lastname)
-    def set_name(self, new_name):
+    @name.setter
+    def name(self, new_name):
         def valid_name(name):
-            if len(name)<3:
+            if len(str(name)) < 3:
                 return False
-            if " " in name:
+            if " " in str(name):
                 return False
-            if not name.alpha()
+            if not str(name).isalpha():
                 return False
-            if name.title() != name:
+            if str(name).title() != str(name):
                 return False
             return True
         if valid_name(new_name):
             self._name = new_name
-        else:
-            raise Exception("Invalid")
-        
-        self._name = new_name
-
 
     @property
     def age(self):
-        return {self._age}
+        return self._age
     
-    @age.setter         # Age is an int type; age > 11 and age < 18
-    def set_age(self, new_age):
-        if type(new_age) != 'int':
-            raise Exception()
-        if new_age < 11 and new_age > 18:
-            raise Exception()
-        else:
+    @age.setter
+    def age(self, new_age):
+        # def valid_age(age):
+        #     if type(age) != 'int':
+        #         return False
+        #     if type(age) == 'str':
+        #         return False
+        #     if (type(age) is int) and (11 < age < 18):
+        #         return True
+        # if valid_age(new_age):
+        #     self._age = new_age
+        if (type(new_age) is int) and (11 < new_age < 18):
             self._age = new_age
+        else:
+            self._age = 13
 
     @property
     def grade(self):
-        return {self._grade}
+        return self._grade
     
-    @grade.setter       # Grade is 9th through 12th; value is formatted with "th"   
-    def set_grade(self, new_grade):
-        if new_grade != 'int':
-            raise Exception()
-        if new_grade < 9 and new_grade > 12:
-            raise Exception()
+    @grade.setter  
+    def grade(self, new_grade):
+        # def valid_grade(grade):
+        #     valid = ["9th", "10th", "11th", "12th"]
+        #     if grade != 'str':
+        #         return "12th"
+        #     if grade not in valid:
+        #         return "12th"
+        #     return True
+        valid = ["9th", "10th", "11th", "12th"]
+        if new_grade in valid:
+            self._grade = new_grade
+        
         else:
-            self._grade = str(new_grade) + "th"
+            self._grade = "12th"
 
     def __str__(self):
-        return f"Student 1: Name: {self._name}, Age: {self._age}, Grade: {self._grade}."
-    
-    #def advance(self):      # years_advanced
+        return (f"Student 1: Name: {self._name}, Age: {self._age}, Grade: {self._grade}.")
 
-        #return f"{self._name} has advanced to the {self._grade} grade."
-    
-    def study(self, subject):
+Student("Alice")
 
-        return f"{self._name} is studying {subject}."
-    
-my_student = Student("Francisco", 13, "12th")
